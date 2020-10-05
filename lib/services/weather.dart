567f177d5828f0f42aa36625aa4bfda9eb471155
Nitesh <niteshwadhwani605@gmail.com>
2020-10-05 19:38:51 +0530

    first commit

---
+++ lib/services/weather.dart
@@ -0,0 +1,56 @@
+import 'package:clima/services/networking.dart';
+import 'package:clima/services/location.dart';
+
+const apiKey = 'c83a2945c797521658335507c7e81e04';
+const openWeatherULR = 'https://api.openweathermap.org/data/2.5/weather';
+
+class WeatherModel {
+
+  Future<dynamic> getCityWeather (String cityName)async{
+    var url = '$openWeatherULR?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  Future<dynamic> getLocationWeather() async {
+    Location location = Location();
+    await location.getCurrentLocaton();
+    NetworkHelper networkHelper = NetworkHelper(
+        '$openWeatherULR?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+  String getWeatherIcon(int id) {
+    if (id < 300) {
+      return '🌩';
+    } else if (id < 400) {
+      return '🌧';
+    } else if (id < 600) {
+      return '☔️';
+    } else if (id < 700) {
+      return '☃️';
+    } else if (id < 800) {
+      return '🌫';
+    } else if (id == 800) {
+      return '☀️';
+    } else if (id <= 804) {
+      return '☁️';
+    } else {
+      return '🤷‍';
+    }
+  }
+
+  String getMessage(int temp) {
+    if (temp > 25) {
+      return 'It\'s 🍦 time';
+    } else if (temp > 20) {
+      return 'Time for shorts and 👕';
+    } else if (temp < 10) {
+      return 'You\'ll need 🧣 and 🧤';
+    } else {
+      return 'Bring a 🧥 just in case';
+    }
+  }
+}
